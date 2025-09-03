e0f30bc8724a4b6106189a3121d052e5e3d8b170
Yogesh Singh <ayogesh1214@gmail.com>
2025-09-03 23:22:13 +0530

    memory game complete

--- memoryGame/src/App.tsx
+++ memoryGame/src/App.tsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
-import "./App.css";
+
 import Layout from "./components/Layout";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
+  const imagesArray = [
+    "https://images.unsplash.com/photo-1756649627237-d367a3e3a311?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1756550120927-dede81d8b2bb?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
+    "https://images.unsplash.com/photo-1756474215990-a18a9a0521d5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8",
+    "https://images.unsplash.com/photo-1756680967174-c0e19cf94f49?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8",
+  ];
   return (
     <>
-      <Layout />
+      <Layout imagesArray={imagesArray} />
     </>
   );
 }
-
-export default App;
--- memoryGame/src/components/Layout.tsx
+++ memoryGame/src/components/Layout.tsx
@@ -1,13 +1,98 @@
 import "../styles/Layout.css";
+import { useState } from "react";
 
-function Layout() {
+interface LayoutProps {
+  imagesArray: string[];
+}
+type Card = {
+  id: number;
+  image: string;
+  isFlipped: boolean;
+  isMatched: boolean;
+};
+function generateShuffleArray(newArray: Card[]): Card[] {
+  for (let i = 0; i < newArray.length; i++) {
+    let shuffledIndex = Math.floor(Math.random() * newArray.length);
+    [newArray[i], newArray[shuffledIndex]] = [
+      newArray[shuffledIndex],
+      newArray[i],
+    ];
+  }
+  return newArray;
+}
+
+function Layout({ imagesArray }: LayoutProps) {
+  const [images, setImages] = useState<Card[]>(() => {
+    let clonedArray = [...imagesArray, ...imagesArray];
+    let newArray: Card[] = clonedArray.map((i, index) => ({
+      id: index,
+      image: i,
+      isFlipped: false,
+      isMatched: false,
+    }));
+    newArray = generateShuffleArray(newArray);
+    return newArray;
+  });
+
+  function flipImage(inNumber: number) {
+    setImages(prevData => {
+      let data = [...prevData];
+      if (data[inNumber].isFlipped || data[inNumber].isMatched) return data;
+
+      data[inNumber] = { ...data[inNumber], isFlipped: true };
+
+      let flippedAndUnmatched = data.filter(i => i.isFlipped && !i.isMatched);
+
+      if (flippedAndUnmatched.length === 2) {
+        if (flippedAndUnmatched[0].image === flippedAndUnmatched[1].image) {
+          data = data.map(card =>
+            card.isFlipped && !card.isMatched
+              ? { ...card, isMatched: true }
+              : card
+          );
+        } else {
+          setTimeout(() => {
+            setImages(prev => {
+              return prev.map(card => {
+                return !card.isMatched ? { ...card, isFlipped: false } : card;
+              });
+            });
+          }, 300);
+        }
+      }
+      return data;
+    });
+  }
+
+  function resetGame() {
+    let clonedArray = [...imagesArray, ...imagesArray];
+    let newArray: Card[] = clonedArray.map((i, index) => ({
+      id: index,
+      image: i,
+      isFlipped: false,
+      isMatched: false,
+    }));
+    newArray = generateShuffleArray(newArray);
+    setImages(newArray);
+  }
   return (
     <div className="skeleton_box">
       <div className="outer_box">
-        {Array.from(new Array(12).fill(0), (value, index) => (
-          <div className="mini_box">{index}</div>
+        {Array.from(images, (value, index) => (
+          <div
+            className={`mini_box ${!value?.isFlipped ? "hide" : ""}`}
+            key={value.id}
+            onClick={() => flipImage(index)}
+          >
+            {!value?.isFlipped ? (
+              <div />
+            ) : (
+              <img src={value.image} height={"100%"} width={"100%"} />
+            )}
+          </div>
         ))}
       </div>
+      <button onClick={resetGame}>Reset Game</button>
     </div>
   );
 }
