6268bd0155808f0d0d453ab279c4f6eb13542647
Yogesh Singh <ayogesh1214@gmail.com>
2025-09-07 20:01:02 +0530

    Scrolltop, clientheight, threshold, scrollheight, react.uievent

--- infiniteScroll/src/components/InfiniteScroll.tsx
+++ infiniteScroll/src/components/InfiniteScroll.tsx
@@ -1,25 +1,44 @@
 import "../styles/InfiniteScroll.css";
 import { useState } from "react";
 
+const THRESHOLD = 20;
 function InfiniteScroll() {
   const [list, setListItems] = useState(() => {
     return [...new Array(140)];
   });
+  const [loading, setLoading] = useState<Boolean>(false);
 
   function loadMore() {
+    setLoading(true);
     setTimeout(() => {
       setListItems(prev => {
-        console.log(prev, "prev", [...prev, ...new Array(100)]);
         return [...prev, ...new Array(100)];
       });
-    }, 400);
+      setLoading(false);
+    }, 1000);
   }
+
+  function handleScroll(event: React.UIEvent<HTMLDivElement>) {
+    let { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
+    let reminginScroll: number = Math.floor(
+      scrollHeight - (clientHeight + scrollTop)
+    ) as number;
+    console.log(reminginScroll, "scroll**");
+    if (reminginScroll < THRESHOLD && !loading) loadMore();
+  }
+
   return (
-    <div>
-      {list.map((item, index) => (
-        <div>{index + 1}</div>
+    <div className="list_layout" onScroll={handleScroll}>
+      {list.map((_, index) => (
+        <div className="item_layout" key={index + 1}>
+          {index + 1}
+        </div>
       ))}
-      <button onClick={loadMore}>Load More</button>
+      {loading ? (
+        <div>.....Loading</div>
+      ) : (
+        <button onClick={loadMore}>Load More</button>
+      )}
     </div>
   );
 }
