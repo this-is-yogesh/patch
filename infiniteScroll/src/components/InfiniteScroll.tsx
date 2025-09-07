e349ec0bf389f1e330ee3b300be26f323d079637
Yogesh Singh <ayogesh1214@gmail.com>
2025-09-07 19:32:16 +0530

    inital 140 and load more

--- infiniteScroll/src/components/InfiniteScroll.tsx
+++ infiniteScroll/src/components/InfiniteScroll.tsx
@@ -1,3 +1,26 @@
+import "../styles/InfiniteScroll.css";
+import { useState } from "react";
+
 function InfiniteScroll() {
-  return <></>;
+  const [list, setListItems] = useState(() => {
+    return [...new Array(140)];
+  });
+
+  function loadMore() {
+    setTimeout(() => {
+      setListItems(prev => {
+        console.log(prev, "prev", [...prev, ...new Array(100)]);
+        return [...prev, ...new Array(100)];
+      });
+    }, 400);
+  }
+  return (
+    <div>
+      {list.map((item, index) => (
+        <div>{index + 1}</div>
+      ))}
+      <button onClick={loadMore}>Load More</button>
+    </div>
+  );
 }
+export default InfiniteScroll;
