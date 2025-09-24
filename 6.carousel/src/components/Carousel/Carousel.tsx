423b3537439e89fb10d8c29c8795de89337f1b77
Yogesh Singh <ayogesh1214@gmail.com>
2025-09-24 14:46:38 +0530

    handlenext

--- 6.carousel/src/components/Carousel/Carousel.tsx
+++ 6.carousel/src/components/Carousel/Carousel.tsx
@@ -1,5 +1,5 @@
 import "../../styles/Carousel.css";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, use } from "react";
 
 type ChildrenProps = {
   children: React.ReactElement[];
@@ -8,13 +8,10 @@ type ChildrenProps = {
 export default function Carousel({ children }: ChildrenProps) {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const childrenRef = useRef<HTMLDivElement | null>(null);
+  const intervalRef = useRef<number>(0);
 
-  useEffect(() => {
-  const childrenRefElement = childrenRef.current;
-  let slidesArray = childrenRefElement?.children;
-  if (slidesArray?.length) {
-    slidesArray[0].setAttribute("data-active", "true");
-  }
+  function startSlide() {
+    const { slidesArray } = getSlides();
     let intervalId = setInterval(() => {
       setCurrentIndex(prev => {
         if (!slidesArray?.length) return 99;
@@ -24,18 +21,50 @@ export default function Carousel({ children }: ChildrenProps) {
         });
         return newIndex;
       });
-    }, 1000);
+    }, 2000);
+    intervalRef.current = intervalId;
+    return intervalId;
+  }
+
+  function getSlides() {
+    const childrenRefElement = childrenRef.current;
+    let slidesArray = childrenRefElement?.children;
+    return { slidesArray };
+  }
+
+  useEffect(() => {
+    let intervalId = startSlide();
     return () => {
       clearInterval(intervalId);
     };
   }, []);
 
+  function handleNext() {
+    const { slidesArray } = getSlides();
+    clearInterval(intervalRef.current);
+    if (!slidesArray?.length) {
+      return;
+    }
+    setCurrentIndex(prev => {
+      let newIndex = prev === slidesArray?.length - 1 ? 0 : prev + 1;
+      [...slidesArray].forEach((slide, index) => {
+        slide.setAttribute("data-active", (index === newIndex).toString());
+      });
+      return newIndex;
+    });
+
+    startSlide();
+  }
   return (
     <>
       {currentIndex}
       <div className="carousel">
         <div className="box" ref={childrenRef}>
           {children}
+        </div>
+        <div className="button__class">
+          <button>＜</button>
+          <button onClick={handleNext}>＞</button>
         </div>
       </div>
     </>
