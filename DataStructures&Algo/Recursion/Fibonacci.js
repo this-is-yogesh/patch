f49bc70305eca854730533d9a4f9dc62e074381c
Yogesh Singh <ayogesh1214@gmail.com>
2025-11-14 11:05:23 +0530

    fib commit

--- DataStructures&Algo/Recursion/Fibonacci.js
+++ DataStructures&Algo/Recursion/Fibonacci.js
@@ -14,7 +14,6 @@ console.log(fibonacci(16));
  * fib of 4 is fib of 3 + fib of 2 = 2 + 1 = 3
  * fib of 5 is fib of 4 + fib of 3 = 3 + 2 = 5
  * hence fib of n is fib of n -1 + fib of n -2
- * fib = of
  * 0,1,1,2,3,5,8,13,21
  * 0,1,2,3,4,5,6,7
  *
