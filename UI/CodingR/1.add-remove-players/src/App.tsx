b834da65367968500693d31d0eb48eb613ab74af
Yogesh Singh <ayogesh1214@gmail.com>
2025-11-15 23:58:52 +0530

    remove players 5

---
+++ UI/CodingR/1.add-remove-players/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+/**
+ * 
+ * lets keep the text field yahi par, aise i will 
+ */
+  return <></>;
+}
+
+export default App;
