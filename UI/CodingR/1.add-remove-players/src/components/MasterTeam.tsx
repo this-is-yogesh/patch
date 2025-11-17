011cdaceded3f6062763230b43a552a491118e24
Yogesh Singh <ayogesh1214@gmail.com>
2025-11-17 17:53:22 +0530

    add delete players wrapping up2

--- UI/CodingR/1.add-remove-players/src/components/MasterTeam.tsx
+++ UI/CodingR/1.add-remove-players/src/components/MasterTeam.tsx
@@ -7,23 +7,26 @@ type masterProps = {
 
 const MasterTeam: React.FC<masterProps> = ({ masterPlayers, del, add }) => {
   const MasterTeamComp = function () {
+    if (!masterPlayers.length) return <></>;
     return (
-      <ul style={{ margin: 0, padding: 10 }}>
-        {masterPlayers.map(player => {
-          return (
-            <li>
-              <span>{player.memberName}</span>
-              <button onClick={() => add(player.id)}>add</button>
-              <button onClick={() => del(player.id)}>delete</button>
-            </li>
-          );
-        })}
-      </ul>
+      <div>
+        <h5>Master Team</h5>
+        <ul style={{ margin: 0, padding: 10 }}>
+          {masterPlayers.map(player => {
+            return (
+              <li>
+                <span>{player.memberName}</span>
+                <button onClick={() => add(player.id)}>add</button>
+                <button onClick={() => del(player.id)}>delete</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     );
   };
   return (
     <div>
-      <h5>Master Team</h5>
       <MasterTeamComp />
     </div>
   );
--- UI/CodingR/1.add-remove-players/src/components/PlayingEleven.tsx
+++ UI/CodingR/1.add-remove-players/src/components/PlayingEleven.tsx
@@ -5,23 +5,22 @@ type masterProps = {
 
 const MasterTeam: React.FC<masterProps> = ({ members }) => {
   const MasterTeamComp = function () {
+    if (!members.length) return <></>;
     return (
-      <ul style={{ margin: 0, padding: 10 }}>
-        {members.map(player => {
-          return (
-            <li>
-              <span>{player.memberName}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <div>
+        <h5>Playing Eleven</h5>
+        <ul style={{ margin: 0, padding: 10 }}>
+          {members.map(player => {
+            return (
+              <li>
+                <span>{player.memberName}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     );
   };
-  return (
-    <div>
-      <h5>Playing Eleven</h5>
-      <MasterTeamComp />
-    </div>
-  );
+  return <MasterTeamComp />;
 };
 export default MasterTeam;
