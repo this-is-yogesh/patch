9b67ecfc91fd7ea22525d8c144ee7fc1ae8f6b34
Yogesh Singh <ayogesh1214@gmail.com>
2025-12-26 12:56:52 +0530

    wrapping up drag and drop

--- UI/CodingR/3.draganddrop/src/components/ListComponent.jsx
+++ UI/CodingR/3.draganddrop/src/components/ListComponent.jsx
@@ -1,11 +1,62 @@
-function ListComponent({ data }) {
-  console.log(data, "data");
+import { useRef } from "react";
+function ListComponent({ data, setInitialData }) {
+  let refTarget = useRef(null);
+  function onDragStartFunction(e, desc) {
+    e.dataTransfer.setData(
+      "application/json",
+      JSON.stringify({
+        name: data[0],
+        desc: desc,
+      })
+    );
+  }
+
+  function onDragOverFunction(e) {
+    e.preventDefault();
+    refTarget.current.style.background = "orange";
+  }
+
+  function onDrop(e, data) {
+
+    const dataDropped = JSON.parse(e.dataTransfer.getData("application/json"));
+    let targetKey = data[0];
+    let parentKey = dataDropped.name;
+    let parentDesc = dataDropped.desc;
+    setInitialData(prev => ({
+      ...prev,
+      [parentKey]: prev[parentKey].filter(data => data !== parentDesc),
+      [targetKey]: [...prev[targetKey], parentDesc],
+    }));
+    refTarget.current.style.background = "lightgrey";
+  }
+
+  function onDragEnter() {
+    // refTarget.current.style.background = "orange";
+  }
+
+  function onDragLeave() {
+    refTarget.current.style.background = "lightgrey";
+  }
   return (
-    <div className="child_comp">
+    <div
+      ref={refTarget}
+      className="child_comp"
+      onDragOver={onDragOverFunction}
+      onDrop={e => onDrop(e, data)}
+      onDragEnter={onDragEnter}
+      onDragLeave={onDragLeave}
+    >
       <h3>{data[0]}</h3>
       <div className="child_body">
-        {data[1].map(d => (
-          <div className="child_description">{d}</div>
+        {data[1].map((desc, index) => (
+          <div
+            className="child_description"
+            key={data[0] + index}
+            draggable
+            onDragStart={e => onDragStartFunction(e, desc)}
+          >
+            {desc}
+          </div>
         ))}
       </div>
     </div>
--- UI/CodingR/3.draganddrop/src/components/ParentComponent.jsx
+++ UI/CodingR/3.draganddrop/src/components/ParentComponent.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
-import "../styles/componentStyles.css"
-import { dataLog as initialData } from "../constants/data";
+import "../styles/componentStyles.css";
+import { dataLog } from "../constants/data";
 import ListComponent from "./ListComponent";
 
-/**
- *
- * parent comp will have all the three comps , and we will fix the layout of all three here
- */
 export default function ParentComponent() {
-  return (
-    <div className="parent_layout">
-      {Object.entries(initialData).map((comp, index) => {
-        return <ListComponent data={comp} key={index} />;
-      })}
-    </div>
-  );
+  const [initialData, setInitialData] = useState(dataLog);
+  function getChildData() {
+    return Object.entries(initialData).map((comp, index) => {
+      return (
+        <ListComponent
+          data={comp}
+          key={index}
+          setInitialData={setInitialData}
+        />
+      );
+    });
+  }
+
+  return <div className="parent_layout">{getChildData()}</div>;
 }
