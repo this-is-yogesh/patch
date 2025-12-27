fc52af48fd51f35e44e973c0f42bb7f538ca0794
Yogesh Singh <ayogesh1214@gmail.com>
2025-12-27 18:58:43 +0530

    wrapping up file explorer

--- UI/CodingR/2.file-explorer/src/components/FileExplorerComponent.tsx
+++ UI/CodingR/2.file-explorer/src/components/FileExplorerComponent.tsx
@@ -1,17 +1,25 @@
 import type { DataType } from "../contants/data";
+import { useState } from "react";
 interface FileExplorerPropsType {
   data: DataType;
 }
 
 export default function FileExplorerComponent({ data }: FileExplorerPropsType) {
+  const [closedStatus, setClosedStatus] = useState(true);
   return (
     <div>
       <div>
-        <span>{data.type === "folder" ? "🗂️ " : "📁 "}</span>
+        <span
+          style={{ cursor: data.type === "folder" ? "pointer" : "" }}
+          onClick={() => setClosedStatus(!closedStatus)}
+        >
+          {data.type === "folder" ? "🗂️ " : "📁 "}
+        </span>
         <span>{data.name}</span>
       </div>
-      <div style={{ paddingLeft: "20px" }}>
+      <div style={{ paddingLeft: "20px", position: "relative" }}>
         {data?.children &&
+          !closedStatus &&
           data.children.map((d, index) => (
             <div key={d.id + index}>
               <FileExplorerComponent data={d} />
